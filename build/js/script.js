39487d50f9f6f423118153e226b3ad85141720f6
amrmaher <amrmaher40@gmail.com>
2021-11-15 02:25:26 +0200

    third commit

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip]').tooltip();
+
+    $('[data-add-to-cart]').click(function(e){
+        alert('اضيف المنتج الي عربة المشتريات');
+        e.stopPropagation();
+    });
 });
